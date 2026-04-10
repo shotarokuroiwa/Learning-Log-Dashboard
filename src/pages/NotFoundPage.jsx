26d7602532f6f4b98e5bb276514d6484335b7261
shotarokuroiwa <shotarokuroiwa@icloud.com>
2026-04-10 21:54:20 +0900

    NotFound画面の実装

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const NotFoundPage = () => {
-    return 
+  return (
+    <div>
+      <h1>404 - ページが見つかりませんでした</h1>
+      <p>お探しのページは存在しないか、移動した可能性があります</p>
+      <Link to="/">トップページへ戻る</Link>
+    </div>
+  ) 
 }
 
 export default NotFoundPage
